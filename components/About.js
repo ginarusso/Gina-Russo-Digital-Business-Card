5e6266e2506740e8422b8d9fee2222168de264a5
Gina Russo <gpr0496@gmail.com>
2022-08-28 17:33:47 -0400

    Update About.js

--- components/About.js
+++ components/About.js
@@ -5,6 +5,7 @@ export default function About() {
             <div className="about">
             <h3>About</h3>
             <p>I am a certified New York State School Library Media & Educational Technology Specialist and a frontend web developer, and believe coding is a literacy - one of the basic fundamentals of learning.</p>
+            <hr />
             </div>
     )
 }
