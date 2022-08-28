5f6a2123245ffc578c86ca574ea971bf26e70269
Gina Russo <gpr0496@gmail.com>
2022-08-28 12:08:07 -0400

    React Digital Business Card
    Uploaded first version

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+            <div className="footer">
+            <a href="https://www.facebook.com/LIFun4Kids" target="_blank"><img src="../images/Facebook-Icon.png" className="footer-img" /></a>
+            <a href="https://github.com/ginarusso" target="_blank"><img src="../images/GitHub-Icon.png" className="footer-img" /></a>
+            <a href="https://www.instagram.com/gpr0496/" target="_blank"><img src="../images/Instagram-Icon.png" className="footer-img" /></a>
+            <a href="https://twitter.com/LIFun4Kids" target="_blank"><img src="../images/Twitter-Icon.png" className="footer-img" /></a>
+            </div>
+    )
+}
