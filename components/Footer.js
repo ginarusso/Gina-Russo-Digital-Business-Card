dac7e8c000d629a6e21ff00fb7a6c4d8c8f9798d
Gina Russo <gpr0496@gmail.com>
2022-08-28 16:58:26 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import React from "react"
 export default function Footer() {
     return (
             <div className="footer">
-            <a href="https://www.facebook.com/LIFun4Kids" target="_blank"><img src="../images/Facebook-Icon.png" className="footer-img" /></a>
-            <a href="https://github.com/ginarusso" target="_blank"><img src="../images/GitHub-Icon.png" className="footer-img" /></a>
-            <a href="https://www.instagram.com/gpr0496/" target="_blank"><img src="../images/Instagram-Icon.png" className="footer-img" /></a>
-            <a href="https://twitter.com/LIFun4Kids" target="_blank"><img src="../images/Twitter-Icon.png" className="footer-img" /></a>
+            <a className="icons" href="https://www.facebook.com/LIFun4Kids" target="_blank"><img src="../images/Facebook-Icon.png" className="footer-img" /></a>
+            <a className="icons" href="https://github.com/ginarusso" target="_blank"><img src="../images/GitHub-Icon.png" className="footer-img" /></a>
+            <a className="icons" href="https://www.instagram.com/gpr0496/" target="_blank"><img src="../images/Instagram-Icon.png" className="footer-img" /></a>
+            <a className="icons" href="https://twitter.com/LIFun4Kids" target="_blank"><img src="../images/Twitter-Icon.png" className="footer-img" /></a>
             </div>
     )
 }
